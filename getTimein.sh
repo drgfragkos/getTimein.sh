3c297e39b2c3c3f310af3bac1a6f55f48926c864
@drgfragkos <drgfragkos@users.noreply.github.com>
2020-04-30 03:14:11 +0400

    Add files via upload

--- getTimein.sh
+++ getTimein.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# @drgfragkos 2019
+# @drgfragkos 2019 ################################################################
 # 
 # getTimein.sh -- Shows the current time in the specified time zone or 
 # geographic zone. Without any argument, this shows UTC/GMT. 
@@ -8,39 +8,62 @@
 # Note that it is possible to match zone directories (regions),
 # but that only time zone files (cities) are valid specifications.
 # 
-# The initial code for this script was found in the book Wicked Cool Shell Scripts. 
-# There were a few mistakes in the original code which prevented it from running succesfully. 
-# These have been corrected and the current version has also been expanded to work under MacOS
-# and use the 'say' feature. 
+# The initial idea for the code for this script was found in the book Wicked Cool 
+# Shell Scripts. There were a few mistakes in the original code which prevented it 
+# from running succesfully. These have been corrected and the current version has 
+# also been expanded to work under MacOS and use the 'say' feature. 
 #
 # Timezones can be found here: /usr/share/zoneinfo/
 # For a list of timezones you can type: sudo systemsetup -listtimezones
-# If a particular city is not listed (e.g. under /usr/share/zoneinfo/Europe/ for Germany the capital Berlin already exists, while Munich is not present). 
-# In that case, copy the Berlin file and rename it to Munich. (Due to the fact that Berlin and Munich will always belong to the exact same timezone)
+# If a particular city is not listed (e.g. under /usr/share/zoneinfo/Europe/ for 
+# Germany the capital Berlin already exists, while Munich is not present). 
+# In that case, copy the Berlin file and rename it to Munich. (Due to the fact that
+# Berlin and Munich will always belong to the exact same timezone)
 # 
-# Each file is a C library. To get the output of the file, you can use one of two commands - zdump or file:
+# Each file is a C library. 
+# To get the output of the file, you can use one of two commands; 'zdump' or 'file' (see examples below):
 #  > zdump /usr/share/zoneinfo/America/New_York
 #  > file /usr/share/zoneinfo/America/New_York
 #
-# Time zone database ref: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Time zone database ref: 
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 #
 # ANSI escape codes: https://en.wikipedia.org/wiki/ANSI_escape_code
 # more info: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-#
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
-#
-# Set Color values to use and the No Color value
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+# more info: http://ascii-table.com/ansi-escape-sequences-vt-100.php
 #
 ###################################################################################
+#
+# Set Color values to use and the No Color value ##################################
+#  Black        0;30     Dark Gray     1;30
+#  Red          0;31     Light Red     1;31
+#  Green        0;32     Light Green   1;32
+#  Brown/Orange 0;33     Yellow        1;33
+#  Blue         0;34     Light Blue    1;34
+#  Purple       0;35     Light Purple  1;35
+#  Cyan         0;36     Light Cyan    1;36
+#  Light Gray   0;37     White         1;37
+#
+RED='\033[0;31m'        # e.g. ${RED} $text ${NC}
+NC='\033[0m'            # No Color
+#
+# Set Bold values to use ##########################################################
+# The possible integers are:
+#   0 - Normal Style     # will cancel any color settings used
+#   1 - Bold
+#   2 - Dim
+#   3 - Italic
+#   4 - Underlined
+#   5 - Blinking
+#   7 - Reverse       # Back color / highlighter
+#   8 - Invisible        
+#
+BoldON="\033[1m"             # ${BoldON}
+BoldOFF="\033[0m"            # ${BoldOFF}  
+#
+###################################################################################
+###################################################################################
+
 
 zonedir="/usr/share/zoneinfo"
 
@@ -58,8 +81,7 @@ if [ $# -eq 0 ] ; then
 elif [ "$1" = "list" ] ; then
   ( echo "All known time zones and regions defined on this system:"
     cd $zonedir
-    find -L * -type f -print | xargs -n 2 | \
-      awk '{ printf "  %-38s %-38s\n", $1, $2 }' 
+    find -L * -type f -print | xargs -n 2 | awk '{ printf "  %-38s %-38s\n", $1, $2 }' 
   ) | more
   exit 0
 else
@@ -71,8 +93,7 @@ else
   # Otherwise we need to dig around a bit to find things. Start by 
   # just counting matches.
 
-  matchcnt="$(find -L $zonedir -name $zone -type f -print |
-        wc -l | sed 's/[^[:digit:]]//g' )"
+  matchcnt="$(find -L $zonedir -name $zone -type f -print | wc -l | sed 's/[^[:digit:]]//g' )"
 
   # Check if at least one file matches
   if [ "$matchcnt" -gt 0 ] ; then
@@ -86,11 +107,8 @@ else
     mixedzone="$zone" 
   else # maybe we can find a matching time zone region, rather than a specific time zone
     # First letter capitalized, rest of word lowercase for region + zone
-    mixedregion="$(echo ${region%${region#?}} \
-                 | tr '[[:lower:]]' '[[:upper:]]')\
-                 $(echo ${region#?} | tr '[[:upper:]]' '[[:lower:]]')"
-    mixedzone="$(echo ${zone%${zone#?}} | tr '[[:lower:]]' '[[:upper:]]') \
-               $(echo ${zone#?} | tr '[[:upper:]]' '[[:lower:]]')"
+    mixedregion="$(echo ${region%${region#?}} | tr '[[:lower:]]' '[[:upper:]]')$(echo ${region#?} | tr '[[:upper:]]' '[[:lower:]]')"
+    mixedzone="$(echo ${zone%${zone#?}} | tr '[[:lower:]]' '[[:upper:]]') $(echo ${zone#?} | tr '[[:upper:]]' '[[:lower:]]')"
     
     if [ "$mixedregion" != "." ] ; then
       # Only look for specified zone in specified region
@@ -127,13 +145,25 @@ clear
 
 ### Alternative Output with Say, adjusted to be printed and sound better. ####
 #
-echo -e It\'s $(TZ=$timezone date '+%A, %e %B %Y, %l:%M %p') in ${RED}$1${NC}             #Formated for better-looking printed output
+echo -e It\'s $(TZ=$timezone date '+%A, %e %B %Y, %l:%M %p') in ${RED}${BoldON}$1${BoldOFF}${NC}             #Formated for better-looking printed output
 
-#rAlt="In  $nicetz  it's  $(TZ=$timezone date '+%A, %e %B %Y, %l:%M %p')"                 #Another option for printing the output
+#rAlt="In  $nicetz  it's  $(TZ=$timezone date '+%A, %e %B %Y, %l:%M %p')"                                    #Another option for printing the output
 #echo $rAlt
 
-say "In $1 it's $(TZ=$timezone date '+%A, %e of %B %Y, %l:%M %p')"                        #Formated for saying it a bit more natural
+if [[ "$OSTYPE" == "darwin"* ]]; then                                                                        #Check for MacOS to use the 'say' feature
+  #echo -e $OSTYPE
+  say "In $1 it's $(TZ=$timezone date '+%A, %e of %B %Y, %l:%M %p')"                                         #Formated for saying it a bit more natural
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo -e $OSTYPE
+  #in case you want to use a text-to-speak command (like eSpeak or similar) under Linux
+  #add your text-to-speach command here#
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  echo -e $OSTYPE
+  # POSIX compatibility layer and Linux environment emulation for Windows
+fi
+
 #
 ####
+
 
 exit 0
